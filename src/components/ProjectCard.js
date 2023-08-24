87ee8ad9dd2b33d8e087d749d440861fade75c2b
Meghan Grousset <megh.g@hotmail.fr>
2023-08-24 15:55:49 +0200

    portfolio v2

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,11 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
-                <img src={imgUrl} />
+                <img style={{
+                    objectFit: 'cover',
+                    height: 220
+                }}
+                    src={imgUrl} />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from '../assets/img/project-img1.png';
-import projImg2 from '../assets/img/project-img2.png';
-import projImg3 from '../assets/img/project-img3.png';
+import projImg1 from '../assets/img/Pico-8.png';
+import projImg2 from '../assets/img/Accueil.png';
+import projImg3 from '../assets/img/Extension.png';
+import projImg4 from '../assets/img/MySafePLace.png';
+import projImg5 from '../assets/img/Antikea.png'
 import colorSharp2 from '../assets/img/color-sharp2.png';
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -30,17 +32,12 @@ export const Projects = () => {
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: projImg1,
+            imgUrl: projImg4,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
+            imgUrl: projImg5,
         },
     ];
 
@@ -53,19 +50,11 @@ export const Projects = () => {
                         <TrackVisibility>
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                                    <h2>Projects</h2>
+                                    <h2>Projets</h2>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                            </Nav.Item>
+
                                         </Nav>
                                         <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                                             <Tab.Pane eventKey="first">
